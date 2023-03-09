561a9913e8f0ad1cc40cec9f064f92ea36cd5de5
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-09 11:46:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,44 @@
 import openai
 import streamlit as st
+from streamlit_chat import message
 
-# Définir les paramètres de l'API OpenAI
-openai.api_key = "YOUR_API_KEY_HERE"
-model_engine = "text-davinci-003"
-
-# Définir la fonction pour obtenir une réponse à partir de l'API OpenAI
-def ask_question(prompt):
-    response = openai.Completion.create(
-        engine=model_engine,
+def generate_response(prompt):
+    completion = openai.Completion.create(
+        engine='text-davinci-003',
         prompt=prompt,
-        max_tokens=10,
+        max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.6,
     )
-    answer = response.choices[0].text.strip()
-    return answer
+    message = completion.choices[0].text
+    return message
 
-# Définir la fonction principale pour l'interface utilisateur
 def main():
-    # Afficher le champ de texte pour la clé API
-    api_key = st.text_input("Entrez votre clé API OpenAI :")
-
-    # Vérifier si la clé API est correcte
-    if api_key:
-        openai.api_key = api_key
-        try:
-            openai.Completion.create(engine=model_engine, prompt="Test prompt", max_tokens=0)
-        except Exception as e:
-            st.error("La clé API est incorrecte. Veuillez entrer une clé valide.")
-            return
-
-        # Si la clé API est correcte, afficher le champ de texte pour la question et la réponse
-        st.success("La clé API est valide. Vous pouvez maintenant poser des questions.")
-        conversation_history = []
-        while True:
-            user_input = st.text_input("Vous :")
-            if not user_input:
-                continue
-            conversation_history.append(f"Vous : {user_input}")
-            bot_response = ask_question("\n".join(conversation_history))
-            conversation_history.append(f"Bot : {bot_response}")
-            st.write(f"Bot : {bot_response}")
-            st.write("\n".join(conversation_history[-10:]))
-            st.write("---")
+    st.title("ChatGPT-like Web App")
+    
+    # Ask the user for their OpenAI API key
+    api_key = st.text_input("Enter your OpenAI API key:")
+    openai.api_key = api_key
+    
+    # Storing the chat
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+    
+    user_input = st.text_input("You:", key='input')
+    
+    if user_input:
+        output = generate_response(user_input)
+        # Store the output
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+    
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 if __name__ == "__main__":
     main()
