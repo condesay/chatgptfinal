b3a491383a2b3444f7a6f647abc7c510206327de
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-09 10:19:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ import streamlit as st
 
 model_engine = "davinci"
 
-def generate_text(prompt, api_key):
-    openai.api_key = api_key
+def is_api_key_valid(api_key):
+    try:
+        openai.api_key = api_key
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt="Hello, World!",
+            max_tokens=2,
+            n=1,
+            stop=None,
+            temperature=1.0,
+        )
+        return True
+    except Exception as e:
+        return False
+
+def generate_text(prompt):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
@@ -19,11 +33,18 @@ def generate_text(prompt, api_key):
 
 def main():
     st.title("OpenAI Chatbot Demo")
-    api_key = st.text_input("Enter your OpenAI API key:")
-    user_input = st.text_input("You:", "")
+    api_key_valid = False
 
+    while not api_key_valid:
+        api_key = st.text_input("Enter your OpenAI API key:")
+        if is_api_key_valid(api_key):
+            openai.api_key = api_key
+            api_key_valid = True
+            st.empty()
+
+    user_input = st.text_input("You:", "")
     if st.button("Send"):
-        bot_response = generate_text(user_input, api_key)
+        bot_response = generate_text(user_input)
         st.text_area("Bot:", value=bot_response, height=200, max_chars=None, key=None)
 
 if __name__ == "__main__":
